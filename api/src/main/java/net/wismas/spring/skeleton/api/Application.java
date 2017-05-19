c5b39340438f364754f72c70eb0a68248187bbaa
Alexis DELAPORTE <contact@alexis-delaporte.com>
2017-05-19 18:38:47 +0200

    deploy configuration

--- api/src/main/java/net/wismas/spring/skeleton/api/Application.java
+++ api/src/main/java/net/wismas/spring/skeleton/api/Application.java
@@ -2,13 +2,20 @@ package net.wismas.spring.skeleton.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  * Created by alexis.delaporte on 15/05/2017.
  */
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
     }
 }
